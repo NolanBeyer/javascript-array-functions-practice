4eb928ce0630d313ad140aa39b308306bacd60a9
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-02-16 23:05:46 -0800

    array method practice

--- worksheet.js
+++ worksheet.js
@@ -31,9 +31,17 @@ const characters = [
 
 //***MAP***
 //1. Get array of all names
+const getAllNames = characters.map((allNames) => allNames.name)
+console.log(getAllNames)
 //2. Get array of all heights
+const getAllHeights = characters.map((allHeights) => allHeights.height)
+console.log(getAllHeights)
 //3. Get array of objects with just name and height properties
+const minifiedRecords = characters.map((character) => ({name: character.name, height: character.height}))
+console.log(minifiedRecords)
 //4. Get array of all first names
+const getAllFirstNames = characters.map((firstNames) => firstNames.name.split(" ")[0])
+console.log(getAllFirstNames)
 
 //***REDUCE***
 //1. Get total mass of all characters
@@ -43,9 +51,17 @@ const characters = [
 
 //***FILTER***
 //1. Get characters with mass greater than 100
+const greaterThanMass = characters.filter((greaterMass) => greaterMass.mass > 100)
+console.log(greaterThanMass)
 //2. Get characters with height less than 200
+const lessThanHeight = characters.filter((lessHeight) => lessHeight.height < 200)
+console.log(lessThanHeight)
 //3. Get all male characters
+const allMaleCharacters = characters.filter((allMales) => allMales.gender === "male")
+console.log(allMaleCharacters)
 //4. Get all female characters
+const allFemaleCharacters = characters.filter((allFemales) => allFemales.gender === "female")
+console.log(allFemaleCharacters)
 
 //***SORT***
 //1. Sort by mass
